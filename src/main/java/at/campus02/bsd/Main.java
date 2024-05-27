65d82c453f12083d6f1a0b1589bca8bfaf7cfa86
Maximilian Schuster <s53558@edu.campus02.at>
2024-05-27 16:47:12 +0200

    start of uebung 5

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,17 +1,26 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
         double num1 = 10.5;
         double num2 = 5.2;
 
+
+        logger.info("Starting the programm");
+
         System.out.println("Addition: " + calculator.add(num1, num2));
         System.out.println("Subtraction: " + calculator.subtract(num1, num2));
+        logger.error("ATTENTION: An error appeared");
         System.out.println("Multiplication: " + calculator.multiply(num1, num2));
         System.out.println("Division: " + calculator.divide(num1, num2));
         System.out.println("Maximilian Schuster");
     }
 }
-
